3ce45e8afc22840535059eae7ed73f8ae929e34b
Li-Bingzi <LiBIngzi_99@icloud.com>
2020-11-08 14:17:47 +0800

    report and cnn-lstm.py(for ref only)

--- code/cnn-lstm.py
+++ code/cnn-lstm.py
@@ -5,8 +5,8 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras import losses, models, optimizers
 from tensorflow.keras.activations import relu, softmax
-from tensorflow.keras.layers import (Dense, Convolution2D, GlobalAveragePooling2D, BatchNormalization, Flatten, Dropout,
-                                     MaxPool2D, Activation, Input)
+from tensorflow.keras.layers import (Dense, Conv2D, GlobalAveragePooling2D, BatchNormalization, Flatten, Dropout,
+                                     MaxPooling2D, Reshape, Activation, Input, LSTM, TimeDistributed)
 
 # sklearn
 from sklearn.metrics import confusion_matrix, accuracy_score
@@ -37,40 +37,47 @@ audio_duration = 2.5
 n_mfcc = 30
 n_spec = 60
 # logspec or mfcc
-data_feature = 'logspec'
+data_feature = 'mfcc'
 only_radvess = False
-
-
-def Conv2DBlock(x):
-    x = Convolution2D(32, (4, 10), padding="same")(x)
-    x = BatchNormalization()(x)
-    x = Activation("relu")(x)
-    x = MaxPool2D()(x)
-    x = Dropout(rate=0.2)(x)
-    return x
 
 
 def get_2d_conv_model(n):
     ''' Create a standard deep 2D convolutional neural network'''
     nclass = 12
-    # 2D matrix of 30 MFCC bands by 216 audio length.
-    inp = Input(shape=(n, 216, 1))
-    print('Each batch: ', inp.shape)
 
-    # Conv blocks
-    x = inp
-    for i in range(4):
-        x = Conv2DBlock(x)
+    # ---------- This is another modelling style -----------#
+    # inp = Input(shape=(n, 216, 1))
+    # print('Each batch: ', inp.shape)
+    # inp = Reshape(target_shape=(216, 30, 1, 1), input_shape=(30, 216, 1))(inp)
+    # x = TimeDistributed(Conv2D(32, (4, 10), padding="same",
+    #                            input_shape=(30, 1, 1)))(inp)
+    # x = TimeDistributed(MaxPooling2D())(x)
+    # x = TimeDistributed(Flatten())(x)
 
-    x = Flatten()(x)
-    x = Dense(64)(x)
-    x = Dropout(rate=0.2)(x)
-    x = BatchNormalization()(x)
-    x = Activation("relu")(x)
-    x = Dropout(rate=0.2)(x)
+    # x = LSTM(20)(x)
+    # x = Dropout(rate=0.2)(x)
+    # x = Dense(64, activation='relu')(x)
+    # x = Dropout(rate=0.2)(x)
+    # out = Dense(nclass, activation='softmax')(x)
+    # model = models.Model(inputs=inp, outputs=out)
 
-    out = Dense(nclass, activation=softmax)(x)
-    model = models.Model(inputs=inp, outputs=out)
+    # define cnn model
+    cnn = Sequential()
+    cnn.add(Input(shape=(30, 216, 1)))
+    cnn.add(Reshape(target_shape=(216, 30, 1, 1), input_shape=(30, 216, 1)))
+    cnn.add(Conv2D(32, (4, 10), padding="same",
+                   input_shape=(30, 1, 1)))
+    cnn.add(MaxPooling2D(pool_size=(1, 2)))
+    cnn.add(Flatten())
+
+    # define LSTM model
+    model = Sequential()
+    model.add(TimeDistributed(cnn))
+    model.add(LSTM(20))
+    model.add(Dropout(rate=0.2))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(rate=0.2))
+    model.add(Dense(nclass, activation='softmax'))
 
     opt = optimizers.Adam(0.001)
     model.compile(
@@ -82,14 +89,14 @@ if __name__ == '__main__':
     # load dataset
 
     extra_name = 'ravdess-' if only_radvess else ''
-    mfcc = np.load(f'./processed_data/{extra_name}{data_feature}.npy')
+    data = np.load(f'./processed_data/{extra_name}{data_feature}.npy')
     ref = pd.read_csv("./metadata/Data_path.csv")
     if only_radvess:
         ref = ref.loc[ref['source'] == 'RAVDESS']
 
     # Split between train and test
     X_train, X_test, y_train, y_test = train_test_split(
-        mfcc, ref.labels, test_size=0.25, shuffle=True, random_state=42)
+        data, ref.labels, test_size=0.25, shuffle=True, random_state=42)
 
     print('X_train: ', X_train.shape)
 
@@ -119,9 +126,9 @@ if __name__ == '__main__':
     # save model and history
     if not os.path.exists('./models'):
         os.mkdir('./models')
-    model.save(f'./models/2DCNN-{extra_name}{data_feature}')
+    model.save(f'./models/2DCNNlstm-{extra_name}{data_feature}')
     if not os.path.exists('./histories'):
         os.mkdir('./histories')
-    hist_path = f'./histories/2DCNN-{extra_name}{data_feature}'
+    hist_path = f'./histories/2DCNNlstm-{extra_name}{data_feature}'
     with open(hist_path, 'wb') as file_pi:
         pickle.dump(model_history.history, file_pi)
